82757c8129cbb408e3d7a406446bfa5b0c1ddd6d
Harsh Gujrati <harshgujrati2003@gmail.com>
2025-12-10 13:36:56 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+//databse connection
+connectDB();
+
+
+app.get('/', (req, res) => {
+    res.send('Food Delivery App Backend is running');
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
